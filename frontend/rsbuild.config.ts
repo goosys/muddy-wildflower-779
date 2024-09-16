4ee9d520c8719e65c8b0f2554f9250a7a51d575c
t.nakamura <shop@goosys.sakura.ne.jp>
2024-09-16 17:15:49 +0000

    add ogp metatags

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -4,6 +4,11 @@ import { pluginReact } from "@rsbuild/plugin-react";
 // https://rsbuild.dev/guide/basic/configure-rsbuild
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    copy: [
+      { from: './src/assets' },
+    ],
+  },
   html: {
     favicon: "src/assets/favicon.ico",
     title({ entryName }) {
@@ -11,6 +16,21 @@ export default defineConfig({
         404: "404 Not Found"
       };
       return titles[entryName] || "Haikunator Generator | Heroku-like memorable random string";
+    },
+    meta: {
+      "og:title": { property: "og:title", content: "Haikunator Generator"},
+      "og:type": { property: "og:type", content: "website"},
+      "og:url": { property: "og:url", content: "https://haikunator-generator.shuttleapp.rs/"},
+      "og:image": { property: "og:image", content: "https://haikunator-generator.shuttleapp.rs/ogp.png"},
+      "og:site_name": { property: "og:site_name", content: "Haikunator Generator"},
+      "og:description": { property: "og:description", content: "Heroku-like memorable random string"},
+      "twitter:card": "summary_large_image",
+      "twitter:description": "Heroku-like memorable random string",
+      "twitter:title": "Haikunator Generator",
+      "twitter:site": "@goosys",
+      "twitter:image": "https://haikunator-generator.shuttleapp.rs/ogp.png",
+      "twitter:creator": "@goosys",
+      description: "Heroku-like memorable random string",
     },
   },
   source: {
